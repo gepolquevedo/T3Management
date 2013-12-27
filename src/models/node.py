db6a8d30355144b25768ec63dd7943123837952a
Gene Paul L. Quevedo <gene.quevedo@gmail.com>
2013-12-27 09:38:44 +0800

    [node] new model node.py

---
+++ src/models/node.py
@@ -0,0 +1,62 @@
+class Node():
+	"""for all Nodes in the system. Instantiate 1 Node depending on the config file"""
+
+class NodeState():
+	"""Abstract Class for states of Node"""
+
+	self.state = None
+
+	def processRequest(self):
+		pass
+
+
+
+
+class NodeStateShuttingDown(NodeState):
+	def __init__():
+		pass
+
+	def cancelShutdown():
+		pass
+
+class NodeStateOn(NodeState):
+	def __init__():
+		pass
+
+	def startShutdown():
+		pass
+
+class NodeStateCountdownStarted(NodeState):
+	pass
+
+class NodeStateOff(NodeState):
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ThinClient():
+	"""Instatiate for each thinclient?
+	"""
+
+
+
+class Switch():
+	pass
+
+class RDPServer():
+	pass
+
+class School():
+	"""contains all instances of the power management system"""
+	pass
